28e0565960078546b212c1e9c560800fbf5b28a0
Jack Moran <jvckmorvn@gmail.com>
2023-12-04 10:48:13 +0200

    issues summary

---
+++ app/IssuesSummary.tsx
@@ -0,0 +1,36 @@
+import { Status } from "@prisma/client";
+import { Card, Flex, Text } from "@radix-ui/themes";
+import Link from "next/link";
+
+interface Props {
+  open: number;
+  inProgress: number;
+  closed: number;
+}
+
+export default function IssuesSummary({ open, inProgress, closed }: Props) {
+  const containers: { label: string; value: number; status: Status }[] = [
+    { label: "Open Issues", value: open, status: "OPEN" },
+    { label: "In Progress Issues", value: open, status: "IN_PROGRESS" },
+    { label: "Closed Issues", value: open, status: "CLOSED" },
+  ];
+  return (
+    <Flex gap="3">
+      {containers.map((container) => (
+        <Card key={container.label}>
+          <Flex direction="column" gap="1">
+            <Link
+              href={`/issues?status=${container.status}`}
+              className="text-small font-medium"
+            >
+              {container.label}
+            </Link>
+            <Text size="5" className="font-bold">
+              {container.value}
+            </Text>
+          </Flex>
+        </Card>
+      ))}
+    </Flex>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,22 @@
-import LatestIssues from "./LatestIssues";
-import Pagination from "./components/Pagination";
+import { PrismaClient } from "@prisma/client";
+import IssuesSummary from "./IssuesSummary";
 
-export default function Home() {
-  return <LatestIssues />;
+export default async function Home() {
+  const prisma = new PrismaClient();
+
+  const openIssues = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgressIssues = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closedIssues = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
+
+  return (
+    <IssuesSummary
+      open={openIssues}
+      inProgress={inProgressIssues}
+      closed={closedIssues}
+    />
+  );
 }
